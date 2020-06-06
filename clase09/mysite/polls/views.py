91c9c566a76d0d0a057a74d9040e68c4a7caad5a
Jhongesell <unmsm182@gmail.com>
2020-06-06 11:14:50 -0500

    agregando archivos en clase09 desde la dual core

--- clase09/mysite/polls/views.py
+++ clase09/mysite/polls/views.py
@@ -4,5 +4,23 @@
 
 from django.http import HttpResponse
 
+from .models import Question
+
+#def index(request):
+#return HttpResponse("Hola a todos, tu estas en el indice de polls")
+
 def index(request):
-    return HttpResponse("Hola a todos, tu estas en el indice de polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("Tu estas viendo la consulta %s." % question_id)
+
+def results(request, question_id):
+    #response = "You're looking at the results of question %s."
+    response = "Tu estas viendo el resultado de la consulta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Tu estas votando por la consulta %s." % question_id)
