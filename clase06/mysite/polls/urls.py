9c4c5fa158550fd2fcb16bab5a748459b09586d7
Jhongesell <unmsm182@gmail.com>
2020-06-06 14:46:26 -0500

    eh modificado según la guía 4

--- clase06/mysite/polls/urls.py
+++ clase06/mysite/polls/urls.py
@@ -3,27 +3,27 @@ from django.urls import path
 from . import views
 
 # Esto fue modificado también el tutorial 1 parte 3
-urlpatterns = [
+#urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     # ex: /polls/9000
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/9000/results
-    path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/9000/vote
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
     # the 'name' value as called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
 
     # added the word 'specifics'
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
-]
+    #path('specifics/<int:question_id>/', views.detail, name='detail'),
+#]
 # Esto fue agregado en el 90% del tutorial N01 parte 3
 app_name = 'polls'
-urlpattern = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id/vote/', views.vote, name='vote'),
-    ]
 
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
